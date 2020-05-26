63b25d7f2cdee3c9642a554b57d09601a8272101
Zil Vyas <zvyas08@gmail.com>
2020-05-26 15:31:56 -0400

    1.5 - fixes to one away

--- arrays_and_strings/one_away.rb
+++ arrays_and_strings/one_away.rb
@@ -4,22 +4,22 @@
 # Problem #1.5
 
 def one_away(str_1, str_2)
-  # Step #1: If the difference between the lengths is greater than 1, then we know we're doing more than 1 delete
   return false if (str_1.length - str_2.length).abs > 1
 
   chars_1 = str_1.split("")
   chars_2 = str_2.split("")
 
-  # Step #2: If one string is 1 char more than the other, then we are doing an insert & comparing chars
   if chars_1.length > chars_2.length
-    chars_1.delete((chars_1 - chars_2).pop) # delete the extra character and compare the rest
+    chars_1.delete((chars_1 - chars_2).pop)     # delete the extra character and compare the rest
+    one_away = (compare(chars_1, chars_2) == 0) # verify the comparison comes back with 0 required edits, because we are doing 1 insert
   elsif chars_1.length < chars_2.length
-    chars_2.delete((chars_2 - chars_1).pop) # delete the extra character and compare the rest
+    chars_2.delete((chars_2 - chars_1).pop)     # delete the extra character and compare the rest
+    one_away = (compare(chars_1, chars_2) == 0) # verify the comparison comes back with 0 required edits, because we are doing 1 insert
+  else
+    one_away = (compare(chars_1, chars_2) <= 1) # if we are not inserting, then just verify we are only making at most 1 edit
   end
 
-  # Step #3: We should be working with equal lengths at this point and we can compare each char
-  one_away = compare(chars_1, chars_2)
-  return one_away <= 1
+  return one_away
 end
 
 def compare(chars_1, chars_2)
@@ -36,7 +36,9 @@ puts one_away('pale', 'ple') # expect true
 puts one_away('pales', 'pale') # expect true
 puts one_away('pal', 'pale') # expect true
 puts one_away('pae', 'pale') # expect true
+puts one_away('pale', 'pile') # expect true
 
+puts one_away('pale', 'piles') # expect false
 puts one_away('pale', 'paless') # expect false
 puts one_away('pale', 'bake') # expect false
 puts one_away('pale', 'bile') # expect false
