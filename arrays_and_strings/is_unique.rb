8be93739cb644f468bb80c64ebe2b9ef7d5bb5ef
Zil Vyas <zvyas08@gmail.com>
2020-05-26 11:59:28 -0400

    change method name for is unique

--- arrays_and_strings/is_unique.rb
+++ arrays_and_strings/is_unique.rb
@@ -3,7 +3,7 @@
 # Author: Zil Vyas
 # Problem #1.1
 
-def unique_characters?(str)
+def is_unique(str)
   chars_map = {}
   str.chars.each do |char|
     if chars_map.has_key?(char)
@@ -15,7 +15,7 @@ def unique_characters?(str)
   return true
 end
 
-puts unique_characters?('aeiou') # expect true
-puts unique_characters?('joeschmoe') # expect false
-puts unique_characters?('alongerstringtotestagainst') # expect false
-puts unique_characters?('saywut') # expect true
+puts is_unique('aeiou') # expect true
+puts is_unique('joeschmoe') # expect false
+puts is_unique('alongerstringtotestagainst') # expect false
+puts is_unique('saywut') # expect true
